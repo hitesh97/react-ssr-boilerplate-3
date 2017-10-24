79d46ec53388b62642ac58894431d83987aeecde
Farbod Salimi <farbod_salimi@yahoo.com>
2017-10-23 19:36:49 -0700

    Fix hydrate warning

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,1 +1,8 @@
-console.log('test');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/home';
+
+ReactDOM.hydrate(
+    <Home />,
+    document.getElementById('root')
+);
