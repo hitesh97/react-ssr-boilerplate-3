33776374a63228ad2c9911adba6bd84afb031650
Farbod Salimi <farbod_salimi@yahoo.com>
2017-10-23 19:15:56 -0700

    Setup client bundle

--- server/src/client/components/home.js
+++ server/src/client/components/home.js
@@ -1,7 +1,12 @@
 import React, { PureCompoenet } from 'react';
 
 const Home = () => {
-    return <div>I'm the home component</div>;
+    return (
+        <div>
+            <div>I'm the home component</div>
+            <button onClick={() => alert('test this')}>Press Me!</button>
+        </div>
+    );
 };
 
 export default Home;
