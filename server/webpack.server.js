32747a531178e305fc67dc29b90d69c3e9984bfa
Farbod Salimi <farbod_salimi@yahoo.com>
2017-10-23 20:56:41 -0700

    Ignore node_modules libraries for the server with Webpack

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
     // Inform webpack that we're building a bundle
@@ -16,7 +17,11 @@ const config = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
+
+    // dont bundle any libraries to our server bundle 
+    // if that library exists inside node_modules folder
+    externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
