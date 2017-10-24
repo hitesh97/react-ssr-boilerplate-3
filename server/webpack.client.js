3153b9d471bc9ea88c81fc8ce1cd00cea84c4643
Farbod Salimi <farbod_salimi@yahoo.com>
2017-10-23 19:44:12 -0700

    Merge webpack config

--- server/webpack.client.js
+++ server/webpack.client.js
@@ -1,6 +1,8 @@
 const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
 
-module.exports = {
+const config = {
 
     // Tell webpack the root filr of our
     // server application
@@ -11,27 +13,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'public')
-    },
-
-    // Tell webpack to run babel on every file it runs through
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_module/,
-                options: {
-                    presets: [
-                        'react',
-                        'stage-0',
-                        ['env', {
-                            targets: {
-                                browsers: ['last 2 versions']
-                            }
-                        }]
-                    ]
-                }
-            }
-        ]
     }
 };
+
+module.exports = merge(baseConfig, config);
