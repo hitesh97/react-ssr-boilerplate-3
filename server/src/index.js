24679332b07acaaf3444780a3286403d42000039
Farbod Salimi <farbod_salimi@yahoo.com>
2017-11-02 00:18:21 -0700

    Add matchRoutes

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/routes';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 
@@ -8,6 +10,8 @@ app.use(express.static('public'));
 
 app.get('*', (req, res) => {
     const store = createStore();
+
+    console.log(matchRoutes(Routes, req.path));
 
     res.send(renderer(req, store));
 });
