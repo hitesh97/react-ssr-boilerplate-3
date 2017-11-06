14452ea990caba02d53c60d5d8f4bbc428e3864b
Farbod Salimi <farbod_salimi@yahoo.com>
2017-11-05 16:55:16 -0800

    add waiting for data load completion

--- server/src/index.js
+++ server/src/index.js
@@ -15,9 +15,10 @@ app.get('*', (req, res) => {
         return route.loadData ? route.loadData(store) : null;
     });
 
-    console.log(promises);
+    Promise.all(promises).then(() => {
+        res.send(renderer(req, store));
+    });
 
-    res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
