f92483e2635b6c0419ae64564b2990a09f9e3c4f
Farbod Salimi <farbod_salimi@yahoo.com>
2017-11-08 22:42:19 -0800

    Add fetchCurrentUser action creator

--- server/public/bundle.js
+++ server/public/bundle.js
@@ -7878,7 +7878,6 @@ Object.defineProperty(exports, "__esModule", {
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 var FETCH_USER = exports.FETCH_USER = 'fetch_users';
-
 var fetchUsers = exports.fetchUsers = function fetchUsers() {
     return function () {
         var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(dispatch, getState, api) {
@@ -7909,6 +7908,41 @@ var fetchUsers = exports.fetchUsers = function fetchUsers() {
 
         return function (_x, _x2, _x3) {
             return _ref.apply(this, arguments);
+        };
+    }();
+};
+
+var FETCH_CURRENT_USER = exports.FETCH_CURRENT_USER = 'fetch_current_user';
+var fetchCurrentUser = exports.fetchCurrentUser = function fetchCurrentUser() {
+    return function () {
+        var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(dispatch, getState, api) {
+            var res;
+            return regeneratorRuntime.wrap(function _callee2$(_context2) {
+                while (1) {
+                    switch (_context2.prev = _context2.next) {
+                        case 0:
+                            _context2.next = 2;
+                            return api.get('/current_user');
+
+                        case 2:
+                            res = _context2.sent;
+
+
+                            dispatch({
+                                type: FETCH_CURRENT_USER,
+                                payload: res
+                            });
+
+                        case 4:
+                        case 'end':
+                            return _context2.stop();
+                    }
+                }
+            }, _callee2, undefined);
+        }));
+
+        return function (_x4, _x5, _x6) {
+            return _ref2.apply(this, arguments);
         };
     }();
 };
--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -1,11 +1,21 @@
 
-export const FETCH_USER = 'fetch_users';
 
+export const FETCH_USER = 'fetch_users';
 export const fetchUsers = () => async (dispatch, getState, api) => {
     const res = await api.get('/users');
 
     dispatch({
         type: FETCH_USER,
+        payload: res
+    });
+};
+
+export const FETCH_CURRENT_USER = 'fetch_current_user';
+export const fetchCurrentUser = () => async (dispatch, getState, api) => {
+    const res = await api.get('/current_user');
+
+    dispatch({
+        type: FETCH_CURRENT_USER,
         payload: res
     });
 };
