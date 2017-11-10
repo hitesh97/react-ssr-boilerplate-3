430c9100aeec10944dcf828355504819e824f9cb
Farbod Salimi <farbod_salimi@yahoo.com>
2017-11-09 22:00:15 -0800

    Add 404 page

--- server/src/client/routes.js
+++ server/src/client/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import App from './app';
 import HomePage from './pages/home';
 import UsersListPage from './pages/users-list';
+import NotFoundPage from './pages/404';
 
 export default [
     {
@@ -16,6 +17,9 @@ export default [
                 ...UsersListPage,
                 path: "/users",
                 exact: true
+            },
+            {
+                ...NotFoundPage
             }
         ]
     }
